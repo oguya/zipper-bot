c41afeaf27486d144b329546727ac85c37fbbc52
Njagi Mwaniki <mwanikibusiness@gmail.com>
2015-02-18 17:22:00 +0300

    Added link-info, history, seen and echo plugins

--- bot.rb
+++ bot.rb
@@ -2,16 +2,30 @@ require 'cinch'
 require_relative 'np'
 require_relative 'messanger'
 require_relative 'wolfram'
-
+require_relative 'echo' 
+require_relative 'seen'
+require_relative 'link_info'
+require_relative 'history'
 
 bot = Cinch::Bot.new do
 	configure do |c|
 		c.nick = "nairobi-bot"
 		c.server = "irc.freenode.net"
-		c.channels = ["#nairobilug"]
+		c.channels = ["#zippy"]
     c.plugins.prefix = /^@/
-		c.plugins.plugins = [Np, Wa]
-#		c.plugins.plugins = [Np, Messanger, Wa]
+		c.plugins.plugins = [Np, Wa, Echo, LinkInfo, History, Seen]
+    #		c.plugins.plugins = [Np, Messanger, Wa]
+    config.plugins.options[History] = {
+     :mode => :max_messages,
+     :max_messages => 10,
+     # :max_age => 5,
+     :time_format => "%H:%M",
+     :file => "/var/cache/seenlog.dat"
+    }
+    config.plugins.options[Seen] = {
+      :file => "/tmp/seenlog.dat"
+    }
+
 	end
 end
 
